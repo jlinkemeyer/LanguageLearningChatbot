2f691e9e6eef7e6d1c3748a053d374fd32ae100f
Lukas Schießer <lschiesser@uos.de>
2022-12-09 15:56:32 +0100

    first commit gpt bot

---
+++ bots/gpt-bot/bot.py
@@ -0,0 +1,63 @@
+from transformers import pipeline, set_seed
+from translate import Translator
+
+
+class Bot:
+
+    name = 'GPT Bot'
+    language ='spanish'
+
+    def __init__(self):
+        self.model = self.load_model()
+        self.esToEn = Translator(from_lang="es", to_lang="en")
+        self.enToEs = Translator(from_lang="en", to_lang="es")
+        self.wordlist = self.load_word_list()
+        self.session = """
+        Leah visits Barcelona as an exchange student. She meets Bot. Both are 13 years old. They plan a picnic with a group of friends.
+        Bot likes to eat patatas bravas and choco frito, but dislikes eating caracoles.
+        Bot is going to bring Tortilla de jamon y queso and horchata to the picnic.
+        
+        They still have to decide for other food. The picnic takes place at 3pm in a park. 
+        
+        Bot: Hi Leah, how are you?
+        
+        """
+
+    def welcome(self):
+        return "Hola, ¿qué tal?"
+
+    def load_word_list(self):
+        with open('data/spanish-word-list.txt', 'r', encoding='utf-8') as f:
+            text = [line.strip() for line in f.readlines()]
+            text.append('Bot:')
+        return text
+
+    def load_model(self):
+        generator = pipeline('text-generation', model='gpt2')
+        set_seed(42)
+        return generator
+    def chat(self, last_user_message, session):
+        translated_message = self.esToEn.translate(last_user_message)
+        self.session += "Leah:" + translated_message + '\n'
+        answer = ''
+        while answer == '':
+            answers = self.model(self.session, max_length=30, num_return_sequences=5)
+            # retranslate
+            translated_answers = []
+            for answer in answers:
+                translated_answers.append(self.enToEs.translate(answer['generated_text']))
+            # filter for words
+
+            for translated_answer in translated_answers:
+                if all(word in self.wordlist for word in translated_answers.split()) and translated_answer.startswith('Bot: '):
+                    answer = translated_answer
+                    break
+            if answer != '':
+                break
+        # remove bot: from answer
+        answer = answer.replace('Bot:', '')
+        # append answer to session
+        self.session += answer + '\n'
+        return answer
+
+
